0c8c237c64cbe443d63806652196ca7dd5b8b663
roshniphartiyal <r30081996@gmail.com>
2016-10-07 18:48:40 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,19 @@ console.log('Loaded!');
 
 //wrong one
 
-document.getElementById('imgid').onclick = function()
+/*document.getElementById('imgid').onclick = function()
 {
     setInterval(moveRight,100);
-};
+};*/
 function moveRight(){
   marginLeft=marginLeft + 10;
   imgid.style.marginLeft = marginLeft + 'px';
 }
+
+var imgid = document.getElementById('imgid');
+ imgid.onlick = function(){
+  imgid.style.marginLeft = '100px';
+ };
+
+
+
