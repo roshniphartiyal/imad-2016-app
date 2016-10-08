6fce9ed37695f70fd389bb39ed392d7f4316471e
roshniphartiyal <r30081996@gmail.com>
2016-10-07 19:41:53 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,27 @@ console.log('Loaded!');
 
 
 var img = document.getElementById('madi');
+
+marginLeft =  0;
+ function moveRight()
+ {
+     marginLeft = marginLeft + 3;
+	 img.style.marginLeft = marginLeft + "px";
+ 
+ }
+
+
+ img.onclick = function(){
+  // one.style.marginLeft = "100px";
+ var interval = setInterval(moveRight,50);
+ }
+
+/*
 img.onclick = function() {
   img.style.marginLeft = '500px';
      
- };
+ }; */
+
 
 
 
