b57669b0c1ef855c5612c3e5f9b24cf0684097d1
roshni phartiyal <r30081996@gmail.com>
2016-11-06 01:18:27 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'rachna.html'));
+  res.sendFile(path.join(__dirname, 'rachna.html'));
 });
 
 app.get('/articleone', function (req, res) {
