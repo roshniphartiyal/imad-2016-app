91d7eab9cd5f6d13059b3fd97c3475ef0a3d1453
roshni phartiyal <r30081996@gmail.com>
2016-11-10 07:32:50 -0800

    Create login.php

---
+++ ui/login.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>
+Login
+</title>
+<link rel="stylesheet" type="text/css" href="a.css">
+</head>
+
+<body>
+
+<div id="login">
+<form action="process.php" method="post">
+<p>
+Login id: <input type="text" name="username">
+</p>
+<p>
+Password: <input type="password" name="password">
+</p>
+<p>
+<input type="submit" id="btn" value="Submit"></center>
+</p>
+</form>
+</div>
+</body>
+</html>
